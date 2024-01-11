272a0c0c484295242d1b094a1ad1c3dccf28c702
Kaushal Chauhan <kaushalchauhan4199@gmail.com>
2024-01-11 16:48:48 +0530

    feat: cart-slider

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,22 @@
-import React from 'react';
+import React, { useContext } from "react";
+import { SidebarContext } from "../contexts/SidebarContext";
+import { BsBag } from "react-icons/bs";
 
 const Header = () => {
-  return <div>Header</div>;
+  const { isOpen, setIsOpen } = useContext(SidebarContext);
+  return (
+    <div>
+      <div>Header</div>
+      <div
+        className="cursor-pointer flex relative"
+        onClick={() => {
+          setIsOpen(!isOpen);
+        }}
+      >
+        <BsBag className="text-2xl" />
+      </div>
+    </div>
+  );
 };
 
 export default Header;
